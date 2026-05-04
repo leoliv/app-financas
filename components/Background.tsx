e9ea8be1eb1bc2a5da858c31932e2f1ad0706993
Leo Oliveira <leoliv95@gmail.com>
2026-05-04 20:03:28 -0300

    feat: pagina criar conta

---
+++ components/Background.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from "react";
+import { KeyboardAvoidingView, Platform, StyleSheet } from "react-native";
+
+type BackgroundProps = {
+  children: ReactNode;
+};
+
+export const Background = ({ children }: BackgroundProps) => {
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+      enabled
+      style={styles.areaContainer}
+    >
+      {children}
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  areaContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#eff5ff",
+  },
+});
