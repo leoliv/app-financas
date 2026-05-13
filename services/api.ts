a4120c1754541b9d48df606ce1fba993b59f2ffd
Leo Oliveira <leoliv95@gmail.com>
2026-05-13 20:58:35 -0300

    fix: corrigindo a segurança

--- services/api.ts
+++ services/api.ts
@@ -1,8 +1,7 @@
 import Axios, { AxiosInstance } from "axios";
-
 const api: AxiosInstance = Axios.create({
   // baseURL: "http://localhost:3333",
-  baseURL: "http://192.168.0.101:3333",
+  baseURL: `http://${process.env.EXPO_PUBLIC_SERVER_IP}:3333`,
 });
 
 export default api;
