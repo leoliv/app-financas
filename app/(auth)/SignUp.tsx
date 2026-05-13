188ade5ce34dbb32cf5652928d3d5f52b07b1024
Leo Oliveira <leoliv95@gmail.com>
2026-05-13 18:30:54 -0300

    feat: ligando o sevidor com axios

--- app/(auth)/SignUp.tsx
+++ app/(auth)/SignUp.tsx
@@ -2,19 +2,38 @@ import { Background } from "@/components/Background";
 import { Button } from "@/components/Button";
 import { CustomInput } from "@/components/CustomInput";
 import { AuthContext } from "@/contexts/auth";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function SignUp() {
-  const { user } = useContext(AuthContext);
+  const { signUp } = useContext(AuthContext);
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <SafeAreaView style={{ flex: 1 }} edges={["top", "right", "left"]}>
       <Background>
-        <CustomInput label="Nome" placeholder="Digite seu nome" />
-        <CustomInput label="E-mail" placeholder="Digite seu email" />
-        <CustomInput label="Senha" placeholder="Digite sua senha" />
+        <CustomInput
+          label="Nome"
+          value={nome}
+          onChangeText={(text) => setNome(text)}
+          placeholder="Digite seu nome"
+        />
+        <CustomInput
+          label="E-mail"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          placeholder="Digite seu email"
+        />
+        <CustomInput
+          label="Senha"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          secureTextEntry={true}
+          placeholder="Digite sua senha"
+        />
         <Button
-          onPress={() => console.log(user)}
+          onPress={() => signUp({ nome, email, password })}
           activeOpacity={0.7}
           label="Registrar"
         />
--- contexts/auth.tsx
+++ contexts/auth.tsx
@@ -1,3 +1,5 @@
+import api from "@/services/api";
+import { router } from "expo-router";
 import React, { createContext, useState } from "react";
 
 interface AuthContextType {
@@ -5,17 +7,35 @@ interface AuthContextType {
     nome: string;
     idade?: number;
   };
+  signUp: ({ nome, email, password }: userProps) => Promise<void>;
 }
+
+type userProps = {
+  nome: string;
+  email: string;
+  password: string;
+};
 
 export const AuthContext = createContext<AuthContextType>({
   user: { nome: "" },
+  signUp: async () => {},
 });
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
+  async function signUp({ nome, email, password }: userProps) {
+    try {
+      const response = await api.post("/users", {
+        name: nome,
+        email: email,
+        password: password,
+      });
+      router.back();
+    } catch (error) {
+      console.error("Deu um error: " + error);
+    }
+  }
   const [user, setUser] = useState({
     nome: "Mateus Ceará",
   });
-  return (
-    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
-  );
+  return <AuthContext.Provider value={{ user, signUp }}>{children}</AuthContext.Provider>;
 }
